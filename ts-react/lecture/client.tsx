50dcd5f06cf5c3f5946c2f1e0828c747c1cc4116
moon-sang-hyeok <asntkdgur04@naver.com>
2024-12-29 00:54:25 +0900

    GuGuDan

---
+++ ts-react/lecture/client.tsx
@@ -0,0 +1,11 @@
+// export default가 아닐 때 * 사용
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+
+import GuGuDan from "./GuGuDan"
+
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root element")
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<GuGuDan />)
